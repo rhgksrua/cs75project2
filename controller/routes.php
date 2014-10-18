9d06410f3ce73a8dc7aa20de85d65e37822cab39
Han Koh <rhgksrua@gmail.com>
2014-10-17 22:21:39 -0500

    added marker name and route color

--- controller/routes.php
+++ controller/routes.php
@@ -16,18 +16,23 @@ if (isset($_GET["route"])) {
     
     if ($xml) {
         $station_in_route = $xml->xpath("/root/routes/route/config/station");
+        $color = $xml->xpath("/root/routes/route/color");
         // get station location
         header("Content-type: text/xml");
         print "<root>";
         print "<stations>";
+        print "<color>{$color[0]}</color>";
         foreach ($station_in_route as $station) {
             print "<station>";
             $st = simplexml_load_file("../model/$station.xml");
+            $name = $st->xpath("/root/stations/station/name");
+            print "<name>{$name[0]}</name>";
             $lat = $st->xpath("/root/stations/station/gtfs_latitude");
             print "<latitude>{$lat[0]}</latitude>";
             $lng = $st->xpath("/root/stations/station/gtfs_longitude");
             print "<longitude>{$lng[0]}</longitude>";
-            //$route_station{} = array($lat, $lng);
+            $abbr = $st->xpath("/root/stations/station/abbr");
+            print "<abbr>{$abbr[0]}</abbr>";
             print "</station>";
 
         }
--- html/js/main.js
+++ html/js/main.js
@@ -1,9 +1,10 @@
 var markers = [];
-var current = "";
+var current = null;
 var map = "";
 var path;
 var infoWindows = [];
 
+// Initial render of Google Maps
 function initialize() {
 
     var mapOptions = {
@@ -11,9 +12,9 @@ function initialize() {
         zoom: 10
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
 }
 
+// Helper function for toggling current route
 function getChildren(n, skipMe) {
     var r = [];
     var elem = null;
@@ -24,129 +25,37 @@ function getChildren(n, skipMe) {
     return r;
 };
 
+// Helper function for toggling current route
 function getSiblings(n) {
     return getChildren(n.parentNode.firstChild, n);
 }
 
 
-
-function addMarker(map, latlng) {
-    for (var i = 0; i < latlng.length; i++) {
-        marker = new google.maps.Marker(
-                {
-                    position: new google.maps.LatLng(latlng[i][0], latlng[i][1]),
-                    title: "I am a marker"
-                });
-
-        marker.setMap(map);
-        markers.push(marker);
-    }
-}
-
-function addPolyline(map, latlng) {
-    var coord = [];
-    var point;
-    for (var i = 0; i < latlng.length; i++) {
-        point = new google.maps.LatLng(latlng[i][0], latlng[i][1]);
-        coord.push(point);
-    }
-    path = new google.maps.Polyline({
-        path: coord,
-         strokeColor: '#FF0000',
-         strokeOpacity: 1.0,
-         strokeWeight: 2
-    });
-
-    path.setMap(map);
-}
-
-function addInfoWindow(map, latlng) {
-    var infoWindow;
-    console.log("inside addInfo");
-    for (var i = 0; i < latlng.length; i++) {
-        infoWindow = new google.maps.InfoWindow({
-            content: "<p>Hellow</p>",
-            //position: new google.maps.LatLng(latlng[i][0], latlng[i][1]),
-        });
-        infoWindows.push(infoWindow);
-        marker = markers[i];
-        google.maps.event.addListener(marker, 'click', function(event) {
-            infoWindow.open(map, this);
-        });
-    }
-
-
-}
-    
-
-
-
-
-function clearMarker() {
-    if (markers.length > 0) {
-        path.setMap(null);
-    }
-    
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-    markers = [];
-
-}
-
-window.onload = function() {
-    var list;
-
-    list = document.getElementById("routes-list");
-
-    list.addEventListener('click', function(event) {
-        if (event.target.id == "routes-list") {
-            return
-        }
-        console.log(event.target.id);
-
-        var sib = getSiblings(event.target);
-
-        // Clear highlights on route name
-        for (var i = 0; i < sib.length; i++) {
-            sib[i].className = "routes";
-        }
-        
-        clearMarker();
-
-        // Toggle highlights and show markers
-        if (event.target.className == "routes") {
-            event.target.className = event.target.className + " highlight";
-            showMap(event.target.id, map);
-        } else if (event.target.className == "routes highlight") {
-            event.target.className = "routes";
-        }
-
-    }, false);
-};
-    
-var showMap = function(route, map) {
+function showMap(route, map) {
 
     var latlngs = [];
+    var lng, lat, name, abbr, color, count, i;
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", "/?page=routes&route=" + route, true);
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                console.log(xhr.responseXML);
                 xmlDoc = xhr.responseXML;
-                
-                var count = xmlDoc.getElementsByTagName("station").length;
+                count = xmlDoc.getElementsByTagName("station").length;
+                for (i = 0; i < count; i++) {
+                    // add station position
+                    lng = xmlDoc.getElementsByTagName("longitude")[i].textContent;
+                    lat = xmlDoc.getElementsByTagName("latitude")[i].textContent;
+                    name = xmlDoc.getElementsByTagName("name")[i].textContent;
+                    abbr = xmlDoc.getElementsByTagName("abbr")[i].textContent;
 
-                for (var i = 0; i < count; i++) {
-
-                    var lng = xmlDoc.getElementsByTagName("longitude")[i].textContent;
-                    var lat = xmlDoc.getElementsByTagName("latitude")[i].textContent;
-                    latlngs.push([lat, lng]);
+                    latlngs.push([lat, lng, name, abbr]);
                 }
+                // color of the route
+                color = xmlDoc.getElementsByTagName("color")[0].textContent;
                 addMarker(map, latlngs);
-                addPolyline(map, latlngs);
+                addPolyline(map, latlngs, color);
                 addInfoWindow(map, latlngs);
                 
             } else {
@@ -158,7 +67,145 @@ var showMap = function(route, map) {
     return markers;
 }
 
+function addMarker(map, latlng) {
+    for (var i = 0; i < latlng.length; i++) {
+        marker = new google.maps.Marker(
+                {
+                    position: new google.maps.LatLng(latlng[i][0], latlng[i][1]),
+                    title: latlng[i][2],
+                });
+        marker.setMap(map);
+        // Store markers for infoWindows
+        markers.push(marker);
+    }
+}
+
+function addPolyline(map, latlng, color) {
+    var coord = [];
+    var point, i;
+
+    for (i = 0; i < latlng.length; i++) {
+        point = new google.maps.LatLng(latlng[i][0], latlng[i][1]);
+        coord.push(point);
+    }
+    path = new google.maps.Polyline({
+        path: coord,
+         strokeColor: color,
+         strokeOpacity: 1.0,
+         strokeWeight: 3
+    });
+    path.setMap(map);
+}
+
+function addInfoWindow(map, latlng) {
+    var infoWindow, i, xhr, contentHTML, count, j, k, dest, estimate;
+    for (i = 0; i < latlng.length; i++) {
+        // Need to store different i value for addListener
+        // function scope
+        (function(i) {
+            marker = markers[i];
+            
+            google.maps.event.addListener(marker, 'click', function(event) {
+                if (current !== null) {
+                    infoWindows[current].close();
+                }
+                current = i;
+                xhr = new XMLHttpRequest();
+
+                console.log(latlng[i][3]);
+                xhr.open("GET", "/?page=eta&station=" + latlng[i][3], true);
+                xhr.onload = function (e) {
+                    if (xhr.readyState === 4) {
+                        if (xhr.status === 200) {
+                            contentHTML = "<div>" + latlng[i][2] + "</div>";
+
+                            // Results from ajax
+                            xmlDoc = xhr.responseXML;
+                            console.log(xmlDoc);
+                            count = xmlDoc.getElementsByTagName("etd").length;
+                            dest = xmlDoc.getElementsByTagName("destination");
+
+                            for (j = 0; j < count; j++) {
+                                contentHTML += "<div>To: " + dest[j].textContent + "</div>";
+                                estimate = dest[j].parentNode.getElementsByTagName("estimate");
+                                contentHTML += "<ul>";
+                                for (k = 0; k < estimate.length; k++) {
+                                    contentHTML += "<li>" + estimate[k].textContent + " minutes</li>";
+                                }
+                                contentHTML += "</ul>";
+                            }
+                            
+                            infoWindow = new google.maps.InfoWindow({
+                                content: contentHTML,
+                            });
+                            infoWindows[i] = infoWindow;
+                            infoWindow.open(map, markers[i]);
+
+                        } else {
+                            console.error(xhr.statusText);
+                        }
+                    }
+                };
+                xhr.send(null);
+            });
+        }(i));
+    }
+}
+    
+// Remove markers
+function clearMarker() {
+    var i;
+
+    // Remove all polylines
+    if (markers.length > 0) {
+        path.setMap(null);
+    }
+    
+    // Remove all markers
+    for (i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
+
+}
+
+window.onload = function() {
+
+    var list, sib, i;
+
+    list = document.getElementById("routes-list");
+
+    // Event delegation
+    list.addEventListener('click', function(event) {
+
+        // Parent node is ignored
+        if (event.target.id == "routes-list") {
+            return
+        }
+
+        // Clear highlights on route name
+        sib = getSiblings(event.target);
+        for (i = 0; i < sib.length; i++) {
+            sib[i].className = "routes";
+        }
+        
+        // Remove all markers when clicked on a route
+        clearMarker();
+
+        // Toggle highlights and show markers
+        if (event.target.className == "routes") {
+            event.target.className = event.target.className + " highlight";
+            // Remove all infoWindows from previous click
+            infoWindows = [];
+            current = null;
+            // Render route
+            showMap(event.target.id, map);
+        } else if (event.target.className == "routes highlight") {
+            event.target.className = "routes";
+        }
+
+    }, false);
+};
+    
+// Render Google Maps
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
